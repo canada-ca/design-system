7bf79171a8f30435f38b06c3ce24a65086b795f8
Borges-Porteous <miguel.borges-porteous@tbs-sct.gc.ca>
2019-12-17 15:45:43 -0500

    URL params for layout variants

--- js/demo-toggles.js
+++ js/demo-toggles.js
@@ -1,70 +1,67 @@
 // document.getElementById('submissionPage').value = location.href;
 $(document).ready(function(){
-var dv = getUrlParameter('dv');
-var lv = getUrlParameter('lv');
-var cv = localStorage.getItem('cv');
+    var dv = getUrlParameter('dv');
+    var lv = getUrlParameter('lv');
+    var cv = localStorage.getItem('cv');
 
-//PRESETS
-if (dv != null) {
-    $('.component-demo').addClass("hidden");
-    $('.component-code').addClass("hidden");
-    $('#'+dv+'-demo').removeClass("hidden");
-    $('#'+dv+'-code').removeClass("hidden");
-    $('#design-variant-menu').val(dv);
-
-    //hide default layout menu
-    //reveal design variant layout menu
-    if (lv !=null){
-        // hide layouts within
-        // reveal specific layout
+    //PRESETS
+    if (dv != null) {
+        $('.component-demo').addClass("hidden");
+        $('.component-code').addClass("hidden");
+        $('.layout-submenu').addClass("hidden");
+        $('#'+dv+'-demo').removeClass("hidden");
+        $('#'+dv+'-code').removeClass("hidden");
+        $('#'+dv+'-submenu').removeClass("hidden");
+        $('#design-variant-menu').val(dv);
+        if (lv !=null){
+            $('#'+dv+'-demo .layout-demo').addClass("hidden");
+            $('#'+dv+'-code .layout-code').addClass("hidden");
+            $('#'+dv+'-demo #'+lv+'-layout').removeClass("hidden");
+            $('#'+dv+'-code #'+lv+'-code').removeClass("hidden");
+        }
     }
-}
 
-if (localStorage.getItem('cv') != null) {
-    $('.code-variant').addClass("hidden");
-    $('#code-variant-menu').val(cv);
-    $('.'+localStorage.getItem('cv')+'-variant').removeClass("hidden");
-}
+    if (localStorage.getItem('cv') != null) {
+        $('.code-variant').addClass("hidden");
+        $('#code-variant-menu').val(cv);
+        $('.'+localStorage.getItem('cv')+'-variant').removeClass("hidden");
+    }
 
-//hide all component demos, show the selected one
-$("#design-variant-menu").change(function(){
-    $('.component-demo').addClass("hidden");
-    $('.component-code').addClass("hidden");
-    $('.layout-submenu').addClass("hidden");
-    $('#'+this.value+'-demo').removeClass("hidden");
-    $('#'+this.value+'-code').removeClass("hidden");
-    $('#'+this.value+'-submenu').removeClass("hidden");
-});
+    //hide all component demos, show the selected one
+    $("#design-variant-menu").change(function(){
+        $('.component-demo').addClass("hidden");
+        $('.component-code').addClass("hidden");
+        $('.layout-submenu').addClass("hidden");
+        $('#'+this.value+'-demo').removeClass("hidden");
+        $('#'+this.value+'-code').removeClass("hidden");
+        $('#'+this.value+'-submenu').removeClass("hidden");
+    });
 
-$(".layout-variant-menu").change(function(){
-    designID = this.id
-    designName = designID.substr(0,designID.indexOf("-"));
-    console.log('#'+designName+'-code #'+this.value+'-code');
-    $('#'+designName+'-demo .layout-demo').addClass("hidden");
-    $('#'+designName+'-code .layout-code').addClass("hidden");
-    $('#'+designName+'-demo #'+this.value+'-layout').removeClass("hidden");
-    $('#'+designName+'-code #'+this.value+'-code').removeClass("hidden");
-});
+    $(".layout-variant-menu").change(function(){
+        designID = this.id
+        designName = designID.substr(0,designID.indexOf("-"));
+        $('#'+designName+'-demo .layout-demo').addClass("hidden");
+        $('#'+designName+'-code .layout-code').addClass("hidden");
+        $('#'+designName+'-demo #'+this.value+'-layout').removeClass("hidden");
+        $('#'+designName+'-code #'+this.value+'-code').removeClass("hidden");
+    });
 
-
-
-
-$("#code-variant-menu").change(function(){
-    $('.code-variant').addClass("hidden");
-    $('.'+this.value+'-variant').removeClass("hidden");
-    localStorage.setItem("cv", this.value);
-});
+    $("#code-variant-menu").change(function(){
+        $('.code-variant').addClass("hidden");
+        $('.'+this.value+'-variant').removeClass("hidden");
+        localStorage.setItem("cv", this.value);
+    });
 });
 
 var getUrlParameter = function getUrlParameter(sParam) {
-var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
         i;
-for (i = 0; i < sURLVariables.length; i++) {
+    for (i = 0; i < sURLVariables.length; i++) {
         sParameterName = sURLVariables[i].split('=');
         if (sParameterName[0] === sParam) {
             return sParameterName[1] === undefined ? true : sParameterName[1];
         }
-}
+    }
 };
